cdc08a3e927921dca51434e288603899649b9e47
gaole <2290672573@qq.com>
2022-06-08 18:12:14 +0800

    ‘完成底部组件的封装’

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <Header></Header>
+    中间部分
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default {
-  name: "header",
+  name: "",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
