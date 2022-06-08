8dc46d15fe6698744b7da2347782f63ebc68f90f
gaole <2290672573@qq.com>
2022-06-08 18:03:13 +0800

    完成头部组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div>
+    <Header></Header>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from "@/components/Header";
+export default {
+  name: "header",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
 </style>
